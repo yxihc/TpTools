2c510ad8d4705cbe119f9d411070b1ce6994d99a
shenkun <shenkun@glavesoft.com>
2018-03-08 16:42:49 +0800

    签名

---
+++ TaoPao-Sample/src/main/java/com/taopao/rxjavaretrofitcutmvp/ui/activity/design/ImageTouActivity.java
@@ -0,0 +1,15 @@
+package com.taopao.rxjavaretrofitcutmvp.ui.activity.design;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.taopao.rxjavaretrofitcutmvp.R;
+
+public class ImageTouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_tou);
+    }
+}
