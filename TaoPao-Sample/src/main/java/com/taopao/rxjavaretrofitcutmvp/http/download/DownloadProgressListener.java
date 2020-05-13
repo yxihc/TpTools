3d6436efdc6b8d47cbddf439b2849b0666bab3e9
shenkun <825210484@qq.com>
2020-05-13 19:08:46 +0800

    架构重调新版优化

--- TaoPao-Sample/src/main/java/com/taopao/rxjavaretrofitcutmvp/http/download/DownloadProgressListener.java
+++
@@ -1,9 +0,0 @@
-package com.taopao.rxjavaretrofitcutmvp.http.download;
-/**
- * @Author: 淘跑
- * @Data: 2018/2/2 13:40
- * @Use:  下载进度listener
- */
-public interface DownloadProgressListener {
-    void update(long bytesRead, long contentLength, boolean done);
-}
--- TaoPao-Sample/src/main/java/com/taopao/rxjavaretrofitcutmvp/http/exception/CustomizeException.java
+++
@@ -1,9 +0,0 @@
-package com.taopao.rxjavaretrofitcutmvp.http.exception;
-
-
-public class CustomizeException extends RuntimeException {
-
-    public CustomizeException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
